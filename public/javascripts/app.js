ab530231b797585781789f0b7704976d0db7635e
Alison Woolstenhulme <aliwoolste@msn.com>
2017-12-01 12:52:36 -0700

    First Commit

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,15 +4,29 @@ angular.module('comment', [])
   function($scope,$http){
    // $scope.test = 'Hello world!';
  $scope.comments = [];
+ $scope.ballot = [];
 $scope.addComment = function() {
-      var newcomment = {title:$scope.formContent,upvotes:0};
+      var newcomment = {title:$scope.formContent,imageUrl:$scope.url,price:$scope.price,upvotes:0};
+      //var newcomment = {title:$scope.formContent,upvotes:0};
+     // var newcomment = {title:$scope.formContent,upvotes:0};
    // if($scope.formContent === '') { return; }
      // console.log("In addComment with "+$scope.formContent);
       //$scope.create({
        // title: $scope.formContent,
        // upvotes: 0,
       //});
+    /*
+    $scope.addProduct = function() {
+      var formContent = {name:$scope.Name,imageUrl:$scope.Url};
+      console.log(formContent);
+      $http.post(formContent); // Send the data to the back end
+      $scope.pokemon.push(formData); // Update the model
+    }
+
+    */
       $scope.formContent='';
+      $scope.price = '';
+      $scope.Url = '';
       $http.post('/comments', newcomment).success(function(data){
         $scope.comments.push(data);
       });
@@ -40,6 +54,23 @@ $scope.addComment = function() {
       $scope.comments.push(data);
     });
   };
+
+
+
+
+ $scope.finalBallot = function() {
+angular.forEach($scope.comments, function(value,key) {
+  if(value.selected) {
+    $scope.upvote(value);
+    $scope.ballot.push(value);
+  }
+});
+ }
+
+
+
+
+
 
     $scope.delete = function(comment) {
       $http.delete('/comments/' + comment._id )
